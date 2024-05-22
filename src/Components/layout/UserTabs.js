df833115e57655dcedd9bb171bf25560439a208d
Shivansh <sgshivansh22@gmail.com>
2024-05-22 17:38:35 +0530

    order page is removed

--- src/Components/layout/UserTabs.js
+++ src/Components/layout/UserTabs.js
@@ -30,9 +30,6 @@ export default function UserTabs({ isAdmin }) {
           >
             Users
           </Link>
-          <Link className={path === "/orders" ? "active" : ""} href={"/orders"}>
-            Orders
-          </Link>
         </>
       )}
     </div>
